511d8495c94d1effc3fd299169eaf5d3bcac4703
Jignesh Patil <patiljignesh@gmail.com>
2021-08-07 18:55:42 +0200

    Basic enum example implemented.

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -1,3 +1,38 @@
 import UIKit
+/*
+ Date: 07 August 2021
+ Author: Jignesh Patil
+ Topic: Enumeration - Enums
+ Def: Are group of related values
+ Notes: Enums has to have cases
+ 
+ */
 
-var greeting = "Hello, playground"
+// Exapmle 1:
+
+enum MobileNetworkProviders {
+    case Vodacome
+    case AirTel
+    case MTN
+    case Virgin
+}
+
+// One of the basic was to use Enums are to use switch statements
+// to so something based on case which is been passed on
+
+func getNetworkRating(on networkProviders: MobileNetworkProviders){
+    switch networkProviders {
+    case .Vodacome:
+        print("Place the code to retrieve rating for Vodacome")
+    case .AirTel:
+        print("Place the code to retrieve rating for AirTel")
+    case .MTN:
+        print("Place the code to retrieve rating for MTN")
+    case .Virgin:
+        print("Place the code to retrieve rating for Virgin")
+    }
+}
+
+// Call the function to retrieve Network Rating
+
+getNetworkRating(on: .AirTel)
