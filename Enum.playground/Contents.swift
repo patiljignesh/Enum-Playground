7f911eb5418b165bd7914a06b37c6c90027757e1
Jignesh Patil <patiljignesh@gmail.com>
2021-08-07 21:17:25 +0200

    Enum 1. Basics 2. CaseInterable 3. Raw Values 4. Associated Values

--- Enum.playground/Contents.swift
+++ Enum.playground/Contents.swift
@@ -8,7 +8,7 @@ import UIKit
  
  */
 
-// Exapmle 1:
+// Exapmle 1: Basic
 
 enum MobileNetworkProviders {
     case Vodacome
@@ -17,7 +17,7 @@ enum MobileNetworkProviders {
     case Virgin
 }
 
-// One of the basic was to use Enums are to use switch statements
+// One of the basic way to use Enums are to use switch statements
 // to so something based on case which is been passed on
 
 func getNetworkRating(on networkProviders: MobileNetworkProviders){
@@ -35,4 +35,71 @@ func getNetworkRating(on networkProviders: MobileNetworkProviders){
 
 // Call the function to retrieve Network Rating
 
-getNetworkRating(on: .AirTel)
+//getNetworkRating(on: .AirTel)
+
+//_______________________________
+
+// Exapmle 2: Raw Values
+// Comment out other enum to prevent duplicate declaration
+
+enum MobileNetworkProvidersReviews: String {
+    case Vodacome   = "Best network"
+    case AirTel     = "It's managable"
+    case MTN        = "Acceptable"
+    case Virgin     = "New but unstable"
+}
+
+// Call the function to retrieve Network Reviews
+
+func getNetworkReviews(on networkProviderReview: MobileNetworkProvidersReviews) {
+    print(networkProviderReview.rawValue)
+}
+
+//getNetworkReviews(on: .MTN)
+
+//_______________________________
+
+// Exapmle 3: CaseIterable
+// Comment out other enum to prevent duplicate declaration
+// Makes the casese in collections
+enum MobileNetworkProvidersReviewsCaseIterable: String, CaseIterable {
+    case Vodacome   = "Best network"
+    case AirTel     = "It's managable"
+    case MTN        = "Acceptable"
+    case Virgin     = "New but unstable"
+}
+
+
+print(MobileNetworkProvidersReviewsCaseIterable.allCases.count)
+
+// Able to access all cases
+for mobileNetwork in MobileNetworkProvidersReviewsCaseIterable.allCases {
+    print(mobileNetwork.rawValue)
+}
+
+//_______________________________
+
+// Exapmle 3: CaseIterable
+// Comment out other enum to prevent duplicate declaration
+// Makes the casese in collections
+enum MobileNetworkProviderSubscribers {
+    case Vodacome (users: Int)
+    case AirTel (subscribers: Int)
+    case MTN
+    case Virgin
+}
+
+
+func getNetworkProviderUserbase(for mobileNetworkProviders: MobileNetworkProviderSubscribers) {
+    switch mobileNetworkProviders {
+    case .Vodacome(let users) where users > 500_000 :
+        print("Follow base is massive!")
+    case .AirTel(let subs) where subs > 100_000 :
+        print("Follow base is okay")
+    case .MTN, .Virgin, .AirTel, .Vodacome :
+        print("Poor follow base")
+    }
+}
+
+getNetworkProviderUserbase(for: .Vodacome(users: 450_000))
+
